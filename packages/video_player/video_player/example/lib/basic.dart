fd8b48298813a916f552b53d09ec156e7f700be8
Matthias <matthias@schaff-smart-solutions.de>
2024-02-08 10:57:10 +0100

    VideoPlayer: ise webm file as remote example

--- packages/video_player/video_player/example/lib/basic.dart
+++ packages/video_player/video_player/example/lib/basic.dart
@@ -28,7 +28,7 @@ class _VideoAppState extends State<VideoApp> {
   void initState() {
     super.initState();
     _controller = VideoPlayerController.networkUrl(Uri.parse(
-        'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4'))
+        'https://test-videos.co.uk/vids/bigbuckbunny/webm/vp8/360/Big_Buck_Bunny_360_10s_20MB.webm'))
       ..initialize().then((_) {
         // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
         setState(() {});
